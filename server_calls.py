ff50b47c3c77d9e83b18a54f60459a7418a37c7c
Paul Nicola <paulnicola@Pauls-MacBook-Pro.local>
2026-03-12 23:25:38 +0200

    Changed UI, Added update user functionallity, added markdown upload section and both pdf and md visualisation

--- server_calls.py
+++ server_calls.py
@@ -4,6 +4,27 @@ import logging
 
 LOGGER = logging.getLogger(__name__)
 
+def login_user(username, password):
+
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+
+    json_data = {
+        'username': username,
+        'password': password,
+        'CV_content': '',
+        'text_summary': '',
+    }
+
+    try:
+        response = requests.post('http://127.0.0.1:8000/loginUser', headers=headers, json=json_data)
+        return response
+    except Exception as e:
+        LOGGER.error(f"Error in register_user: {e}")
+
+
 def register_user(username, hashed_password):
 
     headers = {
@@ -18,6 +39,28 @@ def register_user(username, hashed_password):
 
     try:
         response = requests.post('http://127.0.0.1:8000/registerUser', headers=headers, json=json_data)
-        return response.json()
+        return response.status_code
     except Exception as e:
         LOGGER.error(f"Error in register_user: {e}")
+
+
+def update_user(userID, element, value):
+
+    headers = {
+    'accept': 'application/json',
+    'content-type': 'application/x-www-form-urlencoded',
+}
+
+    params = {
+        'doc_id': userID,
+        'element': element,
+        'value': value,
+    }
+
+    try:
+        response = requests.post('http://127.0.0.1:8000/updateUser', headers=headers, params=params)
+        return response.status_code
+    except Exception as e:
+        LOGGER.error(f"Error in update_user: {e}")
+
+
